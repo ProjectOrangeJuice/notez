9639a64fd8fdf41b0ba4d6c45c1cc8c8c4774bde
Oliver Harris <git@harriso.co.uk>
2018-11-15 21:24:24 +0000

    Added html cleaner to page update

--- sys/editor/updatePage.php
+++ sys/editor/updatePage.php
@@ -9,14 +9,32 @@ if(login_check($conn)){
 
 $loc = $_POST["page"];
 $value = $_POST["content"];
+
+
+
+
+
 if(isset($loc,$value)){
 
 
+  require_once '../clean/HTMLPurifier.standalone.php';
 
-          $se = "UPDATE page SET content=:value WHERE pageId=:page AND userId=:user";
+  $config = HTMLPurifier_Config::createDefault();
+  $config->set('URI.Base', 'http://192.168.1.110'); //Allowed url
+  $config->set("URI.DisableExternal",true);
+  $purifier = new HTMLPurifier($config);
+  $clean_html = $purifier->purify($value);
+  $dirty = "";
+  if($value != $clean_html){
+    $dirty = $value;
+  }
+
+
+          $se = "UPDATE page SET content=:value, dirty=:val WHERE pageId=:page AND userId=:user";
           $stmt = $conn->prepare($se);
           $stmt->bindParam(":value",$value);
           $stmt->bindParam(":page",$loc);
+          $stmt->bindParam(":val",$dirty);
           $stmt->bindParam(":user",$_SESSION["user_id"]);
           $stmt->execute();
           $count = $stmt->rowCount();
